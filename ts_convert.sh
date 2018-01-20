d8e79bc0698db8ab78cc652a6d1aa721471caed4
Sarius <john.mendrake@gmail.com>
2018-01-20 23:23:45 +0100

    fixed a major issue caused by unexpected spaces

--- ts_convert.sh
+++ ts_convert.sh
@@ -2,9 +2,9 @@
 # This script is for converting ts files to mkv using the h265 video and the ac3 audio-codec.
 for i in *.ts
 do
-	ASPECT=`mediainfo "$i" | grep Display | cut -d" " -f25`
-	WIDTH=`mediainfo "$i" | grep Width | cut -d" " -f38`
-        HEIGHT=`mediainfo "$i" | grep Height | cut -d" " -f37`
+	ASPECT=$(mediainfo "$i" | grep Display | cut -d" " -f25)
+	WIDTH=$(mediainfo "$i" | grep Width | tr -dc '0-9')
+        HEIGHT=$(mediainfo "$i" | grep Height | tr -dc '0-9')
 
 	ffmpeg -i "$i" -c:v libx265 -s ${WIDTH}x${HEIGHT} -aspect $ASPECT -c:a ac3 -preset ultrafast -crf 18 "${i%.*}.mkv"
 done

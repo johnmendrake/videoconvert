79c0bc21781cc061cbecc567e989fe882100a2b5
sarius <sarius@erebos.fritz.box>
2020-01-20 09:56:25 +0100

    refoved obsolet files

--- mkbatch.sh
+++ mkbatch.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 # Achtung Windows nutzt Backslashes...
 
-for i in $(find . -name "*.ts"); do
+find . -name "*.ts" >> .tmp
+
+while read i; do
 	echo "$i"
-	#ASPECT=$(mediainfo "$i" | grep Display | cut -d" " -f25)
-	#WIDTH=$(mediainfo "$i" | grep Width | tr -dc '0-9')
-	#HEIGHT=$(mediainfo "$i" | grep Height | tr -dc '0-9')
-	ASPECT="16:9"
-	WIDTH=720
-	HEIGHT=576
+	ASPECT=$(mediainfo "$i" | grep Display | cut -d" " -f25)
+	WIDTH=$(mediainfo "$i" | grep Width | tr -dc '0-9')
+	HEIGHT=$(mediainfo "$i" | grep Height | tr -dc '0-9')
 
 	echo "$ASPECT, $WIDTH, $HEIGHT"
 
 	echo "ffmpeg -i \"$i\" -c:v libx265 -s ${WIDTH}x${HEIGHT} -aspect $ASPECT -c:a ac3 -preset medium -crf 15 \"${i%.*}.mkv\"" | sed 's/\//\\/g' >> "$1"
 
 	echo -e ""
-done
+done < .tmp
+
+rm .tmp
